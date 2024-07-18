6a27594fa7abe6ab058296e32a2e0313a34dd1f9
sostaocperu <sorizano@taocperu.com>
2024-07-17 20:09:59 -0500

    agregando la solicitud de edad

--- ejem01.py
+++ ejem01.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 #Título de la aplicación
 st.title("Ingreso de Datos y Estructuras Condicionales")
+
+#Ingreso de la edad
+edad = st.number_input("Ingrese su edad", min_value=0, max_value=120, step=1)
