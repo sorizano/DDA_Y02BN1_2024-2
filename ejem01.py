a9608798963f7516a3d2ee3bbcbc7614ec3847c3
sostaocperu <sorizano@taocperu.com>
2024-07-17 20:18:22 -0500

    creando la función edad

--- ejem01.py
+++ ejem01.py
@@ -5,3 +5,13 @@ st.title("Ingreso de Datos y Estructuras Condicionales")
 
 #Ingreso de la edad
 edad = st.number_input("Ingrese su edad", min_value=0, max_value=120, step=1)
+
+
+#Función para determinar el mesaje basado en la edad
+def determinar_mensaje(edad):
+    if edad < 18:
+        return "Eres menor de edad."
+    elif 18 <= edad < 65:
+        return "Eres adulto."
+    else:
+        return "Eres adulto mayor."
