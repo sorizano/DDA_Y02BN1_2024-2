f6c5ab1278dfd72384a68bcda4b767b54a36b742
Sergio Orizano Salvador <svorizano@gmail.com>
2024-07-24 19:46:28 -0500

    activando la lectura de archivos excel

--- Eje1_Sem04.py
+++ Eje1_Sem04.py
@@ -3,3 +3,12 @@ import pandas as pd
 
 #Título de la aplicación
 st.title("Análisis de Ventas")
+
+#Subir el archivo Excel
+archivo = st.file_uploader("Cargar Archivo Excel", type=["xlsx"])
+
+if archivo is not None:
+    #Leer el archivo Excel
+    df = pd.read_excel(archivo)
+    st.write("Datos Cargados:")
+    st.write(df)
