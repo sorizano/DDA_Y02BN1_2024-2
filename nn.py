2524e24198e83b7029e7f81c0f3a4be172eb7277
brayan-1a <i2414619@continental.edu.pe>
2024-07-31 21:09:23 -0500

    codigoapi

--- nn.py
+++ nn.py
@@ -2,8 +2,8 @@ import streamlit as st
 from supabase import create_client, Client
 
 # Configurar Supabase
-SUPABASE_URL = "https://dzrqlfnxxtompvovsiso.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cnFsZm54eHRvbXB2b3ZzaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0NzMwMTUsImV4cCI6MjAzODA0OTAxNX0.j4JmBL6IKm1obWB29ytLOZq0_cIvZ8iRL3m3KSzZFQc"
+SUPABASE_URL = "https://clmdobighgagqdqwfclt.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsbWRvYmlnaGdhZ3FkcXdmY2x0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0NzMzMDYsImV4cCI6MjAzODA0OTMwNn0.7pncUo2SvBwi1Jnxl863e9-omO8fGulmZC3_zhUVFTM"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def get_students():
