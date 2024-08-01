24bae94ce0c49f8a73df03cb56b1f453e48d43c0
Sergio Orizano Salvador <svorizano@gmail.com>
2024-07-31 20:05:55 -0500

    creando mi archivo sem06

---
+++ Sem6.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from supabase import creatr_client, Client
+
+#Configurar Supabase
+SUPABASE_URL = "https://dzrqlfnxxtompvovsiso.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cnFsZm54eHRvbXB2b3ZzaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0NzMwMTUsImV4cCI6MjAzODA0OTAxNX0.j4JmBL6IKm1obWB29ytLOZq0_cIvZ8iRL3m3KSzZFQc"
+supabase: Client = create_client(SUPABASE_URL. SUPABASE_KEY)
