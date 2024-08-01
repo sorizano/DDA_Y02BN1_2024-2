9bc8b00f095993b80410fc89dfceb7a943dadbe4
Sergio Orizano Salvador <svorizano@gmail.com>
2024-07-31 20:16:17 -0500

    a ver

--- nn.py
+++ nn.py
@@ -6,11 +6,6 @@ SUPABASE_URL = "https://dzrqlfnxxtompvovsiso.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cnFsZm54eHRvbXB2b3ZzaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0NzMwMTUsImV4cCI6MjAzODA0OTAxNX0.j4JmBL6IKm1obWB29ytLOZq0_cIvZ8iRL3m3KSzZFQc"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-def count_students():
-    response = supabase.table('students').select('*', count='exact').execute()
-    return response.count
-
-
 def get_students():
     response = supabase.table('students').select('*').execute()
     return response.data
