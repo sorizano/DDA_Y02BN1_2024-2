992a05eb619cc23e2ecee3da8dcbd5421c21864f
Sergio Orizano Salvador <svorizano@gmail.com>
2024-07-31 20:10:06 -0500

    coma en lugar de punto lin7

--- Sem6.py
+++ Sem6.py
@@ -4,4 +4,4 @@ from supabase import create_client, Client
 #Configurar Supabase
 SUPABASE_URL = "https://dzrqlfnxxtompvovsiso.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cnFsZm54eHRvbXB2b3ZzaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0NzMwMTUsImV4cCI6MjAzODA0OTAxNX0.j4JmBL6IKm1obWB29ytLOZq0_cIvZ8iRL3m3KSzZFQc"
-supabase: Client = create_client(SUPABASE_URL. SUPABASE_KEY)
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
