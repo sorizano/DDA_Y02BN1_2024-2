c3251b1528fbb43c7bff92f9363b764df1107b8c
brayan-1a <i2414619@continental.edu.pe>
2024-07-31 20:27:17 -0500

    informar

--- Sem6.py
+++ Sem6.py
@@ -5,3 +5,19 @@ from supabase import create_client, Client
 SUPABASE_URL = "https://dzrqlfnxxtompvovsiso.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cnFsZm54eHRvbXB2b3ZzaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0NzMwMTUsImV4cCI6MjAzODA0OTAxNX0.j4JmBL6IKm1obWB29ytLOZq0_cIvZ8iRL3m3KSzZFQc"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+def get_students():
+    response = supabase.table('students').select('*').execute()
+    return response.data
+
+def count_students():
+    response = supabase.table('students').select('*', count='exact').execute()
+    return response.count
+
+def add_student(name, age):
+    supabase.table('students').insert({"name": name, "age": age}).execute()
+
+st.title("CRUD con Streamlit y Supabase")
+
+menu = ["Ver", "Agregar"]
+choice = st.sidebar.selectbox("Menú", menu)
