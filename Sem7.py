5c42b7736bf34897e5c1c869a8cdef97c3a9aafc
Sergio Orizano Salvador <svorizano@gmail.com>
2024-08-07 20:15:05 -0500

    Selección de operaciones

--- Sem7.py
+++ Sem7.py
@@ -8,3 +8,9 @@ SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJ
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 st.title("Sistema de facturación")
+
+#Selección de operación
+option = st.selectbox(
+    '¿Qué operación desea realizar?',
+    ('Consultar Clientes', 'Consultar Productos', 'Generar Factura', 'Ver Facturas')
+)
