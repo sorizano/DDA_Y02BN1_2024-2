de570b637d8e6fff3e165fe5f7157b52df0ac2b8
Sergio Orizano Salvador <svorizano@gmail.com>
2024-08-07 20:29:12 -0500

    mayuscula

--- Sem7.py
+++ Sem7.py
@@ -3,15 +3,15 @@ from supabase import create_client, Client
 import pandas as pd
 
 #Configurar Supabase
-SUPABASE_URL = "https://dzrqlfnxxtompvovsiso.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cnFsZm54eHRvbXB2b3ZzaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0NzMwMTUsImV4cCI6MjAzODA0OTAxNX0.j4JmBL6IKm1obWB29ytLOZq0_cIvZ8iRL3m3KSzZFQc"
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+url = "https://dzrqlfnxxtompvovsiso.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cnFsZm54eHRvbXB2b3ZzaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0NzMwMTUsImV4cCI6MjAzODA0OTAxNX0.j4JmBL6IKm1obWB29ytLOZq0_cIvZ8iRL3m3KSzZFQc"
+supabase: Client = create_client(url, key)
 
 st.title("Sistema de facturación")
 
 #Consulta de Clientes
 st.header("Lista de Clientes")
-clientes = supabase.table('Clientes').select('*').execute()
+clientes = supabase.table('clientes').select('*').execute()
 df_clientes = pd.DataFrame(clientes.data)
 
 #mostrar datos en la interfaz
