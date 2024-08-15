dc44703e0b51bc1f82150c857bc9a27b88290e08
Sergio Orizano Salvador <svorizano@gmail.com>
2024-08-14 20:27:37 -0500

    student

--- Sem8.py
+++ Sem8.py
@@ -1,11 +1,9 @@
 import streamlit as st
 from supabase import create_client, Client
 from fpdf import FPDF
-import requests
-from io import BytesIO
 
 
-#Configurar Supabase
+# Configurar Supabase
 SUPABASE_URL = "https://dzrqlfnxxtompvovsiso.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cnFsZm54eHRvbXB2b3ZzaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0NzMwMTUsImV4cCI6MjAzODA0OTAxNX0.j4JmBL6IKm1obWB29ytLOZq0_cIvZ8iRL3m3KSzZFQc"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
@@ -19,22 +17,22 @@ def count_students():
     response = supabase.table('students').select('*', count='exact').execute()
     return response.count
 
-def generate_pdf(students,student_count):
+def generate_pdf(students, student_count):
     pdf = FPDF()
     pdf.add_page()
 
     pdf.set_font("Arial", size=12)
-    pdf.cell(200,10, txt="Reporte de Estudiantes", ln=True, align='C')
+    pdf.cell(200, 10, txt="Reporte de Estudiantes", ln=True, align='C')
 
     pdf.set_font("Arial", size=10)
-    pdf.cell(200,10, txt=f"Cantidad total de estudiantes: {student_count}", ln=True, align='L')
+    pdf.cell(200, 10, txt=f"Cantidad total de estudiantes: {student_count}", ln=True, align='L')
 
-    pdf.ln(10) #Salto de línea
+    pdf.ln(10)  # Salto de línea
 
     for student in students:
-        pdf.cell(200,10, txt=f"ID: {student['id']}, Nombre: {student['name']}, Edad: {student['age']}", ln=true, align='L')
+        pdf.cell(200, 10, txt=f"ID: {student['id']}, Nombre: {student['name']}, Edad: {student['age']}", ln=True, align='L')
 
-    #Guardar el Pdf
+    # Guardar el PDF
     pdf.output("reporte_estudiantes.pdf")
 
 def download_pdf():
@@ -42,7 +40,7 @@ def download_pdf():
         st.download_button('Descargar reporte en PDF', f, file_name="reporte_estudiantes.pdf")
 
 
-#Streamlit APP
+# Streamlit APP
 st.title("Streamlit y Supabase")
 
 menu = ["Ver", "Agregar", "Actualizar", "Eliminar", "Generar Reporte"]
