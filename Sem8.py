541f728b94a0413745a9a245cb79a6ca6ae89630
Sergio Orizano Salvador <svorizano@gmail.com>
2024-08-14 20:25:09 -0500

    logo_url

--- Sem8.py
+++ Sem8.py
@@ -4,12 +4,12 @@ from fpdf import FPDF
 import requests
 from io import BytesIO
 
+logo_url = "https://taocperu.com/assets/img/taoc_peru_azul.png"
 
 #Configurar Supabase
 SUPABASE_URL = "https://dzrqlfnxxtompvovsiso.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6cnFsZm54eHRvbXB2b3ZzaXNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI0NzMwMTUsImV4cCI6MjAzODA0OTAxNX0.j4JmBL6IKm1obWB29ytLOZq0_cIvZ8iRL3m3KSzZFQc"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
 
 
 def get_students():
